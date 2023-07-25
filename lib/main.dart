aa68066e1847b36990c2ebbe6d2b043a21cfefae
Navid <navidrahman92@gmail.com>
2023-07-25 11:29:03 +0600

    Dark theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,31 @@ import 'package:expense_tracker/widgets/expenses.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125));
+
 void main() {
   runApp(
     SafeArea(
       child: MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
         theme: ThemeData().copyWith(
           useMaterial3: true,
           colorScheme: kColorScheme,
@@ -36,6 +57,7 @@ void main() {
                 ),
               ),
         ),
+        themeMode: ThemeMode.system,
         debugShowCheckedModeBanner: false,
         home: const Expenses(),
       ),
